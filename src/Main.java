3aea33fc24771122d0a44e188cdd7253464215f5
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-05-16 14:30:54 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-@Slf4j
+import java.util.Map;
+import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
-        Log.info("Env Variables : {}", System.getenv());
+        final Logger logger = Logger.getLogger(Main.class.getName());
+        Map<String, String> envVariables = System.getenv();
+        for (Map.Entry<String, String> entry : envVariables.entrySet()) {
+            String variableName = entry.getKey();
+            String variableValue = entry.getValue();
+            logger.info(variableName + "=" + variableValue);
+        }
     }
 }
