8125e8f33d85857cfab61fb6078524717308432f
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-05-20 12:46:42 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ import java.util.Map;
 import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
-        final Logger logger = Logger.getLogger(Main.class.getName());
-        Map<String, String> envVariables = System.getenv();
-        for (Map.Entry<String, String> entry : envVariables.entrySet()) {
-            String variableName = entry.getKey();
-            String variableValue = entry.getValue();
-            logger.info(variableName + "=" + variableValue);
-        }
+        System.out.println("hello world);
     }
 }
